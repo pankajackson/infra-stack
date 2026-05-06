8b61b5bb5244c1fe2ae7d4c45f37dc604d86585d
Pankaj Jackson <pankajackson@live.co.uk>
2026-05-06 18:38:30 +0530

    improved lifecycle

--- proxmox-vm/nodes-lifecycle.tf
+++ proxmox-vm/nodes-lifecycle.tf
@@ -28,3 +28,33 @@ EOT
   }
 
 }
+
+resource "null_resource" "new_worker_lifecycle" {
+  count = local.worker_count
+
+  triggers = {
+    node_name = "lxa-lab-worker-${count.index}-${random_id.worker_node_id[count.index].hex}"
+    private_key = nonsensitive(tls_private_key.ubuntu_vm_key.private_key_pem)
+  }
+
+	provisioner "local-exec" {
+		when = create
+
+		environment = {
+			SSH_KEY = self.triggers.private_key
+		}
+
+		command = <<EOT
+	eval "$(ssh-agent -s)"
+	echo "$SSH_KEY" | ssh-add -
+
+	ssh -o StrictHostKeyChecking=no \
+		-o UserKnownHostsFile=/dev/null \
+		ubuntu@192.168.1.60 \
+  "sudo /usr/local/bin/k8s-node-label.sh ${self.triggers.node_name}"
+
+	ssh-agent -k
+	EOT
+	}
+
+}
