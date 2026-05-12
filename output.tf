8e7cc171f2bedbd0451bb9f1f49c7c1488e4554f
Pankaj Jackson <pankajackson@live.co.uk>
2026-05-13 00:52:18 +0530

    added important outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,22 @@
+output "cluster_name" {
+  value = local.cluster_name
+}
+
+output "cluster_id" {
+  value = local.cluster_id
+}
+
+output "master_ip" {
+  value = local.master_ip
+}
+
+output "worker_ips" {
+  value = [
+    for vm in proxmox_virtual_environment_vm.lxa-k8s-worker :
+    vm.ipv4_addresses[1][0]
+  ]
+}
+
 output "vm_password" {
   value     = random_password.vm_password.result
   sensitive = true
@@ -15,4 +34,16 @@ output "vm_public_key" {
 output "k3s_token" {
   value     = random_id.k3s_token.hex
   sensitive = true
+}
+
+output "kubeconfig_path" {
+  value = "${path.module}/.generated/kubeconfig.yaml"
+}
+
+output "kubeconfig_export_command" {
+  value = "export KUBECONFIG=${path.module}/.generated/kubeconfig.yaml"
+}
+
+output "master_ssh_command" {
+  value = "ssh -i .generated/vm_key.pem ${local.ssh_user}@${local.master_ip}"
 }
